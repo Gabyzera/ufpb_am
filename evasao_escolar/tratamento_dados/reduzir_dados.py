1ce168b36291aaadda55cf7fa9d6515f7e4ad36f
Gabrielly <gabriellysb40028922@gmail.com>
2024-05-08 23:10:03 -0300

    feat: finish neural network && finish decision tree classifier && adjust data associated with predictions

--- evasao_escolar/tratamento_dados/reduzir_dados.py
+++ evasao_escolar/tratamento_dados/reduzir_dados.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
-def reduzir_dados():    
-    ED_BRASILEIRA_2022_PATH = ('tratamento_dados/dados/ed_brasileira_2022.csv')
-    ed_brasileira_2022_df = pd.read_csv(ED_BRASILEIRA_2022_PATH)
+def reduzir_dados(ed_brasileira_df_path, edu_brasileira_redu_df_path):    
+    ed_brasileira_2022_df = pd.read_csv(ed_brasileira_df_path)
     
-    colunas_relevantes = ['taxa_de_abandono_ensino_medio',
+    colunas_relevantes = ['taxa_de_abandono',
+                          'taxa_de_reprovacao',
+                          'regiao',
                           'agua_filtrada',
                           'energia', 
                           'saneamento_basico',
                           'coleta_de_lixo',
+                          'tratamento_de_lixo',
+                          'computador',
+                          'educacao_especial',
+                          'laboratorio_ciencias',
+                          'laboratorio_informatica',
                           'internet',
-                          'alimentacao']
+                          'tipo_de_dependencia',
+                          'biblioteca_sala_leitura',
+                          'tipo_localizacao',
+                          'localizacao_diferenciada',
+                          'alimentacao',
+                          'nenhuma_acessibilidade']
 
-    ed_brasileira_2022_df[colunas_relevantes].to_csv('tratamento_dados/dados/ed_brasileira_2022_redu.csv', index=False)
-    
-    ED_BRASILEIRA_2022_REDU_PATH = ('tratamento_dados/dados/ed_brasileira_2022_redu.csv')
-    ed_brasileira_2022_redu_df = pd.read_csv(ED_BRASILEIRA_2022_REDU_PATH)
+    ed_brasileira_2022_df[colunas_relevantes].to_csv(edu_brasileira_redu_df_path, index=False)
+    ed_brasileira_2022_redu_df = pd.read_csv(edu_brasileira_redu_df_path)
 
     return ed_brasileira_2022_redu_df
